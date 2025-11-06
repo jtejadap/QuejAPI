3eb4d7f75e1be04acb45b1d79ec515ab807e6b78
Juan Tejada <juantejadap@outlook.com>
2025-11-06 13:55:26 -0500

    fix: Update CORS allowed origins to use the correct client URL

--- src/main/java/com/quejapp/quejapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/quejapp/quejapi/configuration/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:4200",
-                "https://quejapp.vercel.app"));
+                "https://quejapp-client.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
